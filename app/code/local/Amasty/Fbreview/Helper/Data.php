64c775f39307cacabb99392e4df8e748c281d15b
OpenShift System User <53091296e0b8cdd4cf00004f@ex-std-node744.prod.rhcloud.com>
2016-11-12 01:39:41 -0500

    Installing Amasty Facebook Review

---
+++ app/code/local/Amasty/Fbreview/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Amasty Team
+ * @copyright Copyright (c) 2016 Amasty (https://www.amasty.com)
+ * @package Amasty_Fbreview
+ */
+
+class Amasty_Fbreview_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
